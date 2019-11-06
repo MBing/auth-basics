6e05f00cc24cbaf2b6e87e14dde7c001c4d50b48
Martin Demiddel <martin.demiddel@quandoo.com>
2019-11-06 16:59:22 +0100

    feat(signup): add user to mongodb

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,5 +1,28 @@
+const User = require('../models/User');
 const signup = (req, res, next) => {
-  res.send({ success: true });
+  const { email, password } = req.body;
+  // check if user with email exists
+  User.findOne({ email: email }, (err, matchingUser) => {
+    if (err) return next(err);
+
+  // return error if email exists
+    if (matchingUser) {
+      return res.status(422).send({ error: 'Email is in use!' });
+    }
+
+    const user = new User({
+      email,
+      password,
+    });
+
+  // save if no user there already
+    user.save(err => {
+        if (err) return next(err)
+
+  // indicate user was created
+        res.json({success: true});
+    });
+  });
 };
 
 module.exports = { signup };
