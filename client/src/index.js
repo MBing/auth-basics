342fbcd454ee0e2283c656e6f99295fba46846c5
Martin Demiddel <martin.demiddel@quandoo.com>
2019-11-13 12:09:12 +0100

    feat(redux): attach redux to our app

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,20 @@ import { App } from './components/App';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Welcome } from './components/Welcome';
 import { Register } from './components/auth/Register';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducers } from './reducers';
+
+const store = createStore(reducers, {});
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route path="/" component={Welcome} />
-      <Route path="/register" component={Register} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" component={Welcome} />
+        <Route path="/register" component={Register} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root')
 );
