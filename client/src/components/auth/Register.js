f69af783a4f2f14b84d173b453a9dfd546d9247b
Martin Demiddel <martin.demiddel@quandoo.com>
2019-11-13 12:17:20 +0100

    feat(redux form): add redux form to register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-class Register extends Component {
+class RegisterContainer extends Component {
   render() {
     return (
       <form>
         <fieldset>
           <label>Email</label>
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
         <fieldset>
           <label>Password</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
       </form>
     );
   }
 }
 
+const Register = reduxForm({ form: 'register' })(RegisterContainer);
 export { Register };
