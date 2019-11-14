64c01507155b2b073fda87266e52bdd7d518f17c
Martin Demiddel <martin.demiddel@quandoo.com>
2019-11-14 10:26:19 +0100

    feat(header): add distinction for authentication

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-class Header extends Component {
+class HeaderContainer extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <div>
+          <Link to="/logout">Log Out</Link>
+          <Link to="/feature">Feature</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Log In</Link>
+        </div>
+      );
+    }
+  }
   render() {
     return (
       <div>
         <Link to="/">Redux Auth</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Log In</Link>
-        <Link to="/logout">Log Out</Link>
-        <Link to="/feature">Feature</Link>
+
+        {this.renderLinks()}
       </div>
     );
   }
 }
 
+const mapStateToProps = state => ({
+  authenticated: state.auth.authenticated,
+});
+
+const Header = connect(mapStateToProps)(HeaderContainer);
 export { Header };
